902d5636280c3d39d78f791a82c8c4d510ee976d
Yevhenbk <balagutrak.jenia@gmail.com>
2022-12-20 13:25:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StopwatchComponent } from './components/stopwatch/stopwatch.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StopwatchComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
