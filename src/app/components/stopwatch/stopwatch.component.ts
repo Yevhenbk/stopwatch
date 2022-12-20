db4a4925f9a028912ac29b5580bbb6d46712a621
Yevhenbk <balagutrak.jenia@gmail.com>
2022-12-21 00:50:57 +0100

    start/stop + reset buttons

--- src/app/components/stopwatch/stopwatch.component.ts
+++ src/app/components/stopwatch/stopwatch.component.ts
@@ -13,15 +13,44 @@ export class StopwatchComponent {
   faRotateLeft: IconDefinition = faRotateLeft;
   faHand: IconDefinition = faHand;
 
+  ms: any = '0' + 0;
+  ss: any = '0' + 0;
+  mm: any = '0' + 0;
+  startTimer: any;
+  running: boolean = false;
+
   start(): void {
-     return console.log('start')
+    if(!this.running) {
+      this.running = true;
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;  
+
+        if(this.ms === 100) {
+          this.ss++;
+          this.ss = this.ss < 10 ? '0' + this.ss : this.ss; 
+          this.ms = '0' + 0;
+        }
+
+        if(this.ss === 60) {
+          this.mm++;
+          this.mm = this.mm < 10 ? '0' + this.mm : this.mm; 
+          this.ss = '0' + 0;
+        }
+      }, 10);
+    } else {
+      this.stop();
+    }
   }
 
   stop(): void {
-    console.log('stop')
+    clearInterval(this.startTimer);
+    this.running = false;
   }
 
   reset(): void {
-    console.log('reset')
+    this.mm = this.ss = this.ms = '0' + 0;
+    clearInterval(this.startTimer);
+    this.running = false;
   }
 }
