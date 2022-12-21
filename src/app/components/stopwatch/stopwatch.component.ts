0ef8f8ff9249cd6a3332945621dca44a271a7567
Yevhenbk <balagutrak.jenia@gmail.com>
2022-12-21 20:00:47 +0100

    change doubleclick button logic 2

--- src/app/components/stopwatch/stopwatch.component.ts
+++ src/app/components/stopwatch/stopwatch.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { faPlay, faPause, faRotateLeft, faHand, 
   IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { fromEvent, Observable } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { fromEvent, Observable, interval } from 'rxjs';
+import { bufferTime, debounceTime, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -15,7 +15,7 @@ export class StopwatchComponent {
   faPause: IconDefinition = faPause;
   faRotateLeft: IconDefinition = faRotateLeft;
   faHand: IconDefinition = faHand;
-  doubleClick$: Observable<Event>;
+  doubleClick$: Observable<Event[]>;
 
   ms: any = '0' + 0;
   ss: any = '0' + 0;
@@ -58,7 +58,7 @@ export class StopwatchComponent {
 
   ngOnInit(): void {
     this.doubleClick$ = fromEvent(document, 'dblclick')
-    .pipe(debounceTime(500))
+    .pipe(bufferTime(500), take(1))
   }
 
   onDoubleClick(): void {
